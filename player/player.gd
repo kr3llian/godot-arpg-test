134d32cc3673ce1e669d5ca758a3f243df83a70f
Ammar Idham <222298680+kr3llian@users.noreply.github.com>
2025-08-11 20:26:27 +0800

    autotiling

--- player/player.gd
+++ player/player.gd
@@ -17,7 +17,7 @@ func updateAnimation():
 		elif velocity.y < 0: direction = "Up"
 		
 		animations.play("walk" + direction)
-	# print(velocity)
+	# print(velocity) # for debugging
 
 func _physics_process(delta: float) -> void:
 	handleInput()
